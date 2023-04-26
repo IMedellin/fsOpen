f2fb4af7adbd97c0d862f46ef187cc838266b9c5
Isaias Medellin <i.j.r.medellin@gmail.com>
2023-04-25 20:27:44 -0500

    added dev backend dependencies && created get/put requests

--- part2/scratch/src/App.js
+++ part2/scratch/src/App.js
@@ -1,34 +1,74 @@
+//dependencies
+import axios from 'axios'
+//components
 import Note from './components/Note'
-
 //Hooks
-import { useState } from 'react'
-const App = (props) => {
-  const { notesArray } = props
+import { useState, useEffect } from 'react'
 
-  const [notes, setNotes] = useState(notesArray)
+const App = () => {
+
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     "Add Note"
   )
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    let ignore = false;
+    axios
+      .get("http://localhost:3001/notes")
+      .then(res => {
+        if (!ignore) {
+          const notes = res.data
+          setNotes(notes)
+        }
+      })
+    return () => {
+      ignore = true;
+    }
+  }, [])
+
+
+
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   const addNote = (event) => {
     event.preventDefault()
-    const newNotes = [...notes,
+    const newNotes =
     {
-      id: notes.length + 1,
       content: newNote,
       important: Math.floor(Math.random() * 10) < 5
-    }]
-    setNotes(newNotes)
-    setNewNote("")
+    }
+
+    axios
+      .post("http://localhost:3001/notes", newNotes)
+      .then(res => {
+        console.log(res.data)
+        setNotes(notes.concat(res.data))
+        setNewNote("")
+      })
+      .catch(err => console.error(err))
   }
+
 
   const handleNoteChange = (e) => {
     let createNewNote = e.target.value
     setNewNote(createNewNote)
   }
+
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios
+      .put(url, changedNote)
+      .then(res => {
+        setNotes(notes.map(n => n.id !== id ? n : res.data))
+      })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -39,7 +79,7 @@ const App = (props) => {
       </select>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
--- part2/scratch/src/components/Note.js
+++ part2/scratch/src/components/Note.js
@@ -1,16 +1,23 @@
 import "./styles/Note.css"
 
-const Note = (props) => {
-  const { note } = props
-
-  if (note.important) {
-    return (
-      <li>{note.content} <em><span className="important">Important</span></em></li>
-    )
-  }
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? 'make not important' : 'make important'
   return (
-    <li>{note.content}</li>
+    <li>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
   )
 }
 
 export default Note
+
+
+// if (note.important) {
+//   return (
+//     <li>{note.content} <em><span className="important">Important</span></em></li>
+//   )
+// }
+// return (
+//   <li>{note.content}</li>
+// )
